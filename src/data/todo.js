c882bd4ce384e1a558a034b8866ee9c3b794ae9e
firecoded <yepyepguru@gmail.com>
2018-08-09 12:02:42 -0700

    created dummydata for todolist

---
+++ src/data/todo.js
@@ -0,0 +1,27 @@
+export default [
+	{
+		title: 'Become React God',
+		details: 'Continue learning React',
+		_id: '001'
+	},
+	{
+		title: 'Work on Final project',
+		details: 'Continue mockups and get dummy data',
+		_id: '002'
+	},
+	{
+		title: 'Groceries',
+		details: 'Buy snacks and Dr. Pepper',
+		_id: '003'
+	},
+	{
+		title: 'Make games',
+		details: 'Make some different games in React',
+		_id: '004'
+	},
+	{
+		title: 'Improve CSS',
+		details: 'Get better at CSS, flex, bootstrap',
+		_id: '005'
+	}
+];
