ae4c5a58c1478863c8d33a81c3f9ced06a3dcb08
firecoded <yepyepguru@gmail.com>
2018-08-09 17:17:40 -0700

    axios practice

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 import Todo_list from './todo_list';
 import AddItem from "./add_item";
 import listData from '../data/todo';
+import axios from 'axios';
 
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=c618_fire	';
 
 
 class App extends Component {
 	constructor(props){
-		super(props)
+		super(props);
 
 		this.state = {
 			items: []
@@ -19,31 +22,44 @@ class App extends Component {
 		this.getListData();
 	}
 	
-	addItem = (item) => {
-		item._id = new Date().getTime();
-		this.setState({
-			items: [item, ...this.state.items]
-		})
+	async addItem (item) {
+		try {
+			if(!item.title){
+				throw new Error('Missing Title'); //throw is like return, stops when hit
+			}
+			await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+		} catch(err){
+			console.log(err)
+		}
+		this.getListData();
 	}
 
-	getListData(){
-		//this is where would make the server call
-
+	async getListData() {
+		const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
 		this.setState({
-			items: listData
+			items: resp.data.todos
 		});
-
 	}
+
 	render(){
-		console.log(this.state)
 		return (
 		    <div className= "container">
 		        <h1 className="center">To Do List</h1>
-		        <AddItem add={this.addItem}/>
+		        <AddItem add={this.addItem.bind(this)}/>
 		        <Todo_list list = {this.state.items}/>
 		    </div>
 		);
 	}
 }		
 
+// getListData(){
+	// 	axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+			
+	// 		this.setState({
+	// 			items: resp.data.todos
+	// 		});
+
+	// 	}).catch(err => {
+	// 		console.log("error", err.message)
+	// 	});
 export default App;
